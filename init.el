304528e62e44fba2265858fec9644fbbc0f78579
Rutherther <rutherther@proton.me>
2024-06-19 18:40:42 +0200

    feat: add ace-window to golden-ratio commands

--- init.el
+++ init.el
@@ -461,26 +461,33 @@
   (golden-ratio-exclude-modes '("ediff-mode"))
   ;; Work with evil
   (golden-ratio-extra-commands '(
-    evil-window-left
-    evil-window-right
-    evil-window-up
-    evil-window-down
-    buf-move-left
-    buf-move-right
-    buf-move-up
-    buf-move-down
-    window-number-select
-    select-window
-    select-window-1
-    select-window-2
-    select-window-3
-    select-window-4
-    select-window-5
-    select-window-6
-    select-window-7
-    select-window-8
-    select-window-9))
-
+                                 ;; ace-window
+                                 ace-window
+                                 ace-swap-window
+                                 ace-delete-window
+                                 ace-select-window
+                                 ;; magit
+                                 ;; TODO: but there is something...
+                                 ;; evil
+                                 evil-window-left
+                                 evil-window-right
+                                 evil-window-up
+                                 evil-window-down
+                                 buf-move-left
+                                 buf-move-right
+                                 buf-move-up
+                                 buf-move-down
+                                 window-number-select
+                                 select-window
+                                 select-window-1
+                                 select-window-2
+                                 select-window-3
+                                 select-window-4
+                                 select-window-5
+                                 select-window-6
+                                 select-window-7
+                                 select-window-8
+                                 select-window-9))
   ;; Work with which-key
   (golden-ratio-inhibit-functions '(
                                     (lambda () (and which-key--buffer
